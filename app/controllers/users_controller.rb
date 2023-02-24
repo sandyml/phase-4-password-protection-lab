cb26e86f3088c0632cd094df4491f63c8617293e
sandym <sandramayla@gmail.com>
2023-02-24 11:23:28 -0500

    commit tree 20b8b5d7e9d08c1be8c0c8588110d94cc4b8781b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,46 +1,128 @@
+# class UsersController < ApplicationController
+#     before_action :authorize, only: [:show]
+#     # rescue_from ActiveRecord::RecordInvalid, with: :user_data_invalid
+#     # rescue_from ActiveRecord::RecordNotFound, with: :user_data_not_found
+
+#  # def create
+#  #  new_user = User.create!(user_params)
+#  #  session[:user_id] ||= new_user.id
+#  #  render json: new_user, status: :created
+#  # end
+ 
+#  def create 
+#   user = User.create(user_params)
+#    if user.valid? 
+#     session[:user_id] = user.id
+#     render json: user, status: :created
+#    else
+#     render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+#    end
+#  end
+
+#  def show
+#     user = User.find_by(id: session[:user_id])
+#     render json: user, status: :ok
+#  end
+
+#  private
+
+#  def user_params
+#     params.permit(:username, :password, :password_confirmation)
+#  end
+
+#  def authorize
+#     return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+#  end
+
+#  # def user_data_invalid(error_hash)
+#  #     render json: { errors: error_hash.record.errors.full_messages }, status: :unprocessable_entity
+#  # end
+
+#  # def user_data_not_found
+#  #     render json: { error: "Please login to view your account page." }, status: :not_found
+#  # end
+# end
+
+# class UsersController < ApplicationController
+#    rescue_from ActiveRecord::RecordInvalid, with: :user_data_invalid
+#    rescue_from ActiveRecord::RecordNotFound, with: :user_data_not_found
+
+#    def create
+#        new_user = User.create!(user_params)
+#        session[:user_id] ||= new_user.id
+#        render json: new_user, status: :created
+#    end
+
+#    # def create 
+#    #    user = User.create(user_params)
+#    #    if user.valid? 
+#    #       session[:user_id] = user.id
+#    #       render json: user, status: :created
+#    #    else
+#    #       render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+#    #    end
+#    # end
+
+#    # def create
+#    #    user = User.create(user_params)
+#    #    if user.valid?
+#    #      session[:user_id] = user.id
+#    #      render json: user, status: :created
+#    #    else
+#    #      render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+#    #    end
+#    #  end
+
+#    def show
+#        render json: User.find(session[:user_id]), status: :ok
+#    end
+
+#    # def show
+#    #    user = User.find_by(id: session[:user_id])
+#    #    render json: user
+#    #  end
+
+#    private
+
+#    def user_params
+#        params.permit(:username, :password, :password_confirmation, :user)
+#    end
+
+#    def user_data_invalid(error_hash)
+#        render json: { errors: error_hash.record.errors.full_messages }, status: :unprocessable_entity
+#    end
+
+#    def user_data_not_found
+#        render json: { error: "Please login to view your account page." }, status: :not_found
+#    end
+# end
+
 class UsersController < ApplicationController
-    before_action :authorize, only: [:show]
-    # rescue_from ActiveRecord::RecordInvalid, with: :user_data_invalid
-    # rescue_from ActiveRecord::RecordNotFound, with: :user_data_not_found
-
- # def create
- #  new_user = User.create!(user_params)
- #  session[:user_id] ||= new_user.id
- #  render json: new_user, status: :created
- # end
-
- def create 
-  user = User.create(user_params)
-   if user.valid? 
-    session[:user_id] = user.id
-    render json: user, status: :created
-   else
-    render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+   before_action :authorize, only: [:show]
+ 
+   def create
+     user = User.create(user_params)
+     if user.valid?
+       session[:user_id] = user.id
+       render json: user, status: :created
+     else
+       render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+     end
    end
+ 
+   def show
+     user = User.find_by(id: session[:user_id])
+     render json: user
+   end
+ 
+   private
+ 
+   def authorize
+     return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+   end
+ 
+   def user_params
+     params.permit(:username, :password, :password_confirmation)
+   end
+ 
  end
-
- def show
-    user = User.find_by(id: session[:user_id])
-    render json: user, status: :ok
- end
-
- private
-
- def user_params
-    params.permit(:username, :password, :password_confirmation)
- end
-
- def authorize
-    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
- end
-
- # def user_data_invalid(error_hash)
- #     render json: { errors: error_hash.record.errors.full_messages }, status: :unprocessable_entity
- # end
-
- # def user_data_not_found
- #     render json: { error: "Please login to view your account page." }, status: :not_found
- # end
-
-
-end
